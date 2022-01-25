6a7895c8939e188f26bd80abb276bcc6c9d2014d
4nth0ny1 <acatullo4@gmail.com>
2022-01-25 10:50:00 -0600

    tried to work out bugs but still persisting

--- queue.js
+++ queue.js
@@ -1,16 +1,26 @@
+const allCallers = document.querySelector('.all-callers');
+const position = document.querySelector('.position');
+const enter = document.querySelector('.enter');
+const exit = document.querySelector('.exit');
+const callerName = document.querySelector('.name-input');
+const whoIsFirst = document.querySelector('.who-is-first');
+
 function CallSystem () {
     collection = [];
     this.print = () => {
         console.log(collection);
     }
 
-    this.enqueue = (e) => {
-        const addNewCaller = collection.push(e.target.textContent);
-        allCallers.append(addNewCaller);
+    this.enqueue = (element) => {
+        console.log(element);
+        // const addNewCaller = collection.push(e.target.textContent);
+        const newCaller = collection.push(element);
+        allCallers.append(collection.push(newCaller));
     }
 
     this.dequeue = () => {
-        // we want to remove someone everytime the attendent is done with someone. we'll say that is every minute. 
+        console.log('you clicked the dequeue button');
+        // // we want to remove someone everytime the attendent is done with someone. we'll say that is every minute. 
         return collection.shift();
     } 
 
@@ -21,22 +31,17 @@ function CallSystem () {
 
     this.front = () => {
         return collection[0];
+        whoIsFirst.append(collection[0]);
+
     }
 }
 
 const callsys = new CallSystem();
-
-const allCallers = document.querySelector('.all-callers');
-const position = document.querySelector('.position');
-const enter = document.querySelector('.enter');
-const exit = document.querySelector('.exit');
-const callerName = document.querySelector('.name-input');
-
-enter.addEventListener('click', callsys.enqueue);
-
 callsys.enqueue('Anthony');
 callsys.enqueue('Susan');
-
+callsys.enqueue('Jim');
+callsys.enqueue('Debolina');
+callsys.enqueue('Kavya');
 // callsys.dequeue();
 console.log(callsys.print());
 console.log(callsys.size());
