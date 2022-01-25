3a88a66b16d9382f457607d778dd683ace556a8b
4nth0ny1 <acatullo4@gmail.com>
2022-01-25 10:42:01 -0600

    add styles to html and css

--- queue.js
+++ queue.js
@@ -4,9 +4,9 @@ function CallSystem () {
         console.log(collection);
     }
 
-    this.enqueue = (element) => {
-        collection.push(element);
-        allCallers.append(collection);
+    this.enqueue = (e) => {
+        const addNewCaller = collection.push(e.target.textContent);
+        allCallers.append(addNewCaller);
     }
 
     this.dequeue = () => {
@@ -14,30 +14,30 @@ function CallSystem () {
         return collection.shift();
     } 
 
-    // this.size = () => {
-    //     return collection.length;
-    // }
+    this.size = () => {
+        return collection.length;
+        position.append(collection.length);
+    }
 
-    // this.front = () => {
-    //     return collection[0];
-    // }
+    this.front = () => {
+        return collection[0];
+    }
 }
+
+const callsys = new CallSystem();
 
 const allCallers = document.querySelector('.all-callers');
 const position = document.querySelector('.position');
 const enter = document.querySelector('.enter');
 const exit = document.querySelector('.exit');
+const callerName = document.querySelector('.name-input');
 
-enter.addEventListener('click', CallSystem.enqueue);
+enter.addEventListener('click', callsys.enqueue);
 
-const callsys = new CallSystem();
 callsys.enqueue('Anthony');
 callsys.enqueue('Susan');
-callsys.enqueue('Debolina');
-callsys.enqueue('Raj');
-callsys.enqueue('Kumar');
-callsys.enqueue('Steven');
-callsys.dequeue();
+
+// callsys.dequeue();
 console.log(callsys.print());
 console.log(callsys.size());
 console.log(callsys.front());
